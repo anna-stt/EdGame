689e6bddfd5cebd9260d0cace4a15ab3643c1602
anna-stt <161654454+anna-stt@users.noreply.github.com>
2024-11-21 08:43:32 -0300

    structs.h

---
+++ structs.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+typedef struct No {
+    struct No* opcao1;
+    struct No* opcao2;
+} No;
+
+typedef struct Jogador{
+  vida = 6
+  //sorte, talvez colocar para definir se a escolha vai funcionar ou n
+  //nome, se ler um arquivo da pra adicionar <name> e colocar nome
+}Jogador;
